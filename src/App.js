679ee4f86f6a6c524159eef6e37dc59265d4a165
Suban <076bct082.suban@pcampus.edu.np>
2021-03-27 22:36:25 +0545

    Name Changed and Added changing of mode(child component doesn't update)

--- src/App.js
+++ src/App.js
@@ -5,10 +5,63 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Timer/>
+        <Pomodoro/>
       </header>
     </div>
   );
+}
+
+class ModeForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: props.currentMode};
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (event) => {
+    this.setState({value: parseInt(event.target.value)});
+    this.props.handleModeChange(parseInt(event.target.value));
+  }
+
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+          Pick your favorite flavor:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value={1}>Timer</option>
+            <option value={2}>Pom</option>
+           </select>
+         </label>
+        </form>
+      </div>
+    );
+  }
+}
+
+class Pomodoro extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      mode: localStorage.getItem("Pomodoro_Mode") || 1,
+    }
+    // this.handleModeChange = this.handleModeChange.bind(this);
+  }
+  handleModeChange = (newMode) => {
+    this.setState({mode: newMode,});
+  }
+
+  render(){
+    return (
+      <div>      
+        <ModeForm currentMode={this.state.mode} handleModeChange={(newMode) => this.handleModeChange(newMode)}/>
+        <Timer mode={this.state.mode}/>
+        {this.state.mode}
+      </div>
+    )
+  }
 }
 let cron;
 class Timer extends React.Component {
@@ -17,12 +70,11 @@ class Timer extends React.Component {
     this.state = {
       running: false,
       started: false,
-      clock: 0,
-      time: "00 : 00"
-
+      clock: ((this.props.mode === 1) ? 0 : 1500),
+      time: ((this.props.mode === 1) ? "00 : 00" : "25 : 00"),
     }
-
   }
+
   handleClick(){
     if(this.state.running){
       this.Pause();
@@ -30,12 +82,23 @@ class Timer extends React.Component {
     }else{
       this.Pause()
       cron = setInterval(() => {this.Update(); }, 1000);
-      this.setState({running: true});
+      this.setState({running: true, started: true});
+      return;
     }
-    
+  }
+
+  Reset(){
+    if(this.props.mode === 1)
+      this.setState({time: "00 : 00", clock: 0, running: false});
+    else
+      this.setState({time: "25 : 00", clock: 0, running: false});
+    this.Pause();
   }
   Update(){
-    this.setState({clock: (this.state.clock + 1), });
+    if(this.props.mode === 1)
+      this.setState({clock: (this.state.clock + 1), });
+    else
+      this.setState({clock: (this.state.clock - 1), });
     let values = Array(2).fill(0);
     let clock = this.state.clock;
 
@@ -53,14 +116,15 @@ class Timer extends React.Component {
     this.setState({running: false});
   }
   render(){
-
-    
     return (
       <div>
-        {this.state.running}
       <Clock time={this.state.time} onClick={() => this.handleClick()}/>
+      {
+        !this.state.running && this.state.started
+        ? <Reset reset={()=> this.Reset()}/>
+        : <p>Click on the timer to begin.</p>
+      }
       </div>
-
     );
   }
 }
@@ -71,9 +135,31 @@ function Clock(props){
       backgroundColor: "#282c39",
       padding: "10px",
       fontFamily: "Arial",
+      cursor: "pointer",
+      fontSize: "4em",
       
     };
-  return <h1 style={mystyle} onClick={props.onClick}>{props.time}</h1>
+  return <p style={mystyle} onClick={props.onClick}>{props.time}</p>
 }
+function Reset(props){
+  const mystyle = {
+      color: "white",
+      backgroundColor: "#202135",
+      fontFamily: "Arial",
+      fontSize: "0.8em",
+      borderRadius: "4px", 
+      height: "2em",
+      width: "6em",
+      borderColor: "#202135",
+      outline: "none",
+    }
+  return (
+    <div>
+      <button style={mystyle} onClick={() => props.reset()}>Reset</button>
+    </div>
+  )
+}
+
+
 export default App;
 
